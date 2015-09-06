8debbeb493e8cbe5050a7e41449a412de2df22e4
Mathew Kleppin <matkle414@gmail.com>
2015-09-05 21:43:17 -0500

    commit gh-pages

--- material-avatar.js
+++ material-avatar.js
@@ -162,13 +162,11 @@
 
   Avatar.prototype.generateColor = function (index) {
     var _defaults = [
-        '#1abc9c', '#2ecc71', '#3498db',
-        '#9b59b6', '#34495e', '#16a085',
-        '#27ae60', '#2980b9', '#8e44ad',
-        '#2c3e50', '#f1c40f', '#e67e22',
-        '#e74c3c', '#95a5a6', '#f39c12',
-        '#d35400', '#c0392b', '#bdc3c7',
-        '#7f8c8d'
+        '#303F9F', '#3F51B5', '#C5CAE9',
+        '#FFC107', '#727272',
+        '#00796B', '#009688', '#009688',
+        '#536DFE', '#FFA000', '#FFEB3B',
+        '#FFECB3'
       ];
 
     //Uses the randomColor generator - https://github.com/davidmerfield/randomColor
